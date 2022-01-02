d7c5bef0c47ac35f5217ca143008515572d9f269
juliettkhar <juliettkhar@gmail.com>
2022-01-02 16:58:27 +0300

    add ignore,storage service

---
+++ services/log.service.js
@@ -0,0 +1,24 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js'
+
+const printError = err => {
+    console.log(chalk.bgRed(`ERROR ${err}`))
+}
+
+const printSuccess = message => {
+    console.log(chalk.bgGreen(`SUCCESS ${message}`))
+}
+
+const printHelp = () => {
+    console.log(
+        dedent`
+            ${chalk.bgCyan(`HELP`)}
+            Без параметров - вывод погоды
+            -s [CITY] - установка города
+            -h - помощь
+            -t [API_KEY] - сохранение токена
+        `
+    )
+}
+
+export { printSuccess, printHelp, printError }
